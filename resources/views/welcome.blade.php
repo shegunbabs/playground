73da2aaa36077aa1774891f58c0863397c782e80
Shegun Babs <shegun.babs@gmail>
2023-01-21 16:37:11 +0000

    subdomain route fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Albert+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
         <title>Challenge Landing Page</title>
-        @vite(['resources/css/app.css', 'resources/css/app.js'])
+        @vite(["resources/css/app.css"])
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
@@ -44,7 +44,7 @@
                             <div class="flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-500"><path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" /></svg>
                                 <div class="ml-4 text-lg leading-7 font-semibold">
-                                    <a href="{{ config("app.url") }}/best-western/landing" class="underline text-gray-900 dark:text-white">
+                                    <a href="{{ route("bw.landing") }}" class="underline text-gray-900 dark:text-white">
                                         BestWestern Hospital Landing (Health Challenge)
                                     </a>
                                 </div>
